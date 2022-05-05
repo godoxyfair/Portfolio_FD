f1eb0fb7857d4ef831cc255a33fc718ba42bd977
godoxyfair <godoxyfair@gmail.com>
2022-05-05 18:01:29 +0300

    new css

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function mypPopClose(){
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += " responsive";
+        x.className += "responsive";
     } else {
         x.className = "topnav";
     }
